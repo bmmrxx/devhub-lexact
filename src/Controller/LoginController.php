686da5ab3d2463c4cb7fb0904dc18cd7ae1a6d86
bmmrxx <mitzbo@outlook.com>
2025-03-13 14:53:32 +0000

    Controllers & templates

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'login')]
+    public function loginPage(): Response
+    {
+        return $this->render(
+            'login.html.twig',
+        );
+    }
+}
---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProfileController extends AbstractController
+{
+    #[Route('/profile', name: 'profile')]
+    public function loginPage(): Response
+    {
+        return $this->render(
+            'profile.html.twig',
+        );
+    }
+}
