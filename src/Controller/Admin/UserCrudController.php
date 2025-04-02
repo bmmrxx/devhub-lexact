fd50e7bb5cdaf0ccc8f67318971de686d1bf2192
bmmrxx <mitzbo@outlook.com>
2025-04-02 20:37:23 +0000

    Project CRUD & CRUD admin

---
+++ src/Controller/Admin/UserCrudController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller\Admin;
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use App\Enum\UserRoleEnum;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class UserCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('User')
+            ->setEntityLabelInPlural('Users')
+            ->setSearchFields(['email', 'name'])
+            ->setDefaultSort(['created_at' => 'DESC']);
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        yield TextField::new('name');
+        yield EmailField::new('email');
+        yield TextField::new('password')->onlyOnForms();
+
+        yield ChoiceField::new('roles')
+            ->setChoices($this->getRoleChoices())
+            ->allowMultipleChoices()
+            ->renderExpanded()
+            ->renderAsBadges([
+                'ROLE_ADMIN' => 'success',
+                'ROLE_INTERN' => 'primary',
+                'ROLE_MENTOR' => 'warning',
+            ]);
+
+        yield DateTimeField::new('created_at')
+            ->hideOnForm();
+    }
+
+    private function getRoleChoices(): array
+    {
+        $choices = [];
+        foreach (UserRoleEnum::cases() as $role) {
+            $choices[$role->value] = $role->value;
+        }
+        return $choices;
+    }
+}
--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -2,17 +2,86 @@
 
 namespace App\Controller;
 
+use App\Entity\Resources\Project;
+use App\Form\ProjectType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class ProjectController extends AbstractController
+class ProjectController extends AbstractController
 {
-    #[Route('/project', name: 'app_project')]
-    public function index(): Response
+    #[Route('/project', name: 'project_index', methods: ['GET'])]
+    public function index(EntityManagerInterface $em): Response
     {
+        $projects = $em->getRepository(Project::class)->findBy(
+            ['user' => $this->getUser()],
+        );
+
         return $this->render('project/index.html.twig', [
-            'controller_name' => 'ProjectController',
+            'projects' => $projects,
         ]);
+    }
+
+    #[Route('/project/new', name: 'project_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $project = new Project();
+        $project->setUser($this->getUser());
+
+        $form = $this->createForm(ProjectType::class, $project);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($project);
+            $em->flush();
+
+            $this->addFlash('success', 'Project "' . $project->getName() . '" is aangemaakt!');
+            return $this->redirectToRoute('project_show', ['id' => $project->getId()]);
+        }
+
+        return $this->render('project/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/project/{id}', name: 'project_show', methods: ['GET'])]
+    public function show(Project $project): Response
+    {
+        return $this->render('project/show.html.twig', [
+            'project' => $project,
+        ]);
+    }
+
+    #[Route('/project/{id}/edit', name: 'project_edit', methods: ['GET', 'POST'])]
+    public function edit(Project $project, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(ProjectType::class, $project);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            $this->addFlash('success', 'Project succesvol bijgewerkt!');
+            return $this->redirectToRoute('project_show', ['id' => $project->getId()]);
+        }
+
+        return $this->render('project/edit.html.twig', [
+            'project' => $project,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/project/{id}/delete', name: 'project_delete', methods: ['POST'])]
+    public function delete(Project $project, Request $request, EntityManagerInterface $em): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $project->getId(), $request->request->get('_token'))) {
+            $em->remove($project);
+            $em->flush();
+            $this->addFlash('success', 'Project succesvol verwijderd!');
+        }
+
+        return $this->redirectToRoute('project_index');
     }
 }
